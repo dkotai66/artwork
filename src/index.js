67de0ea03aa2a063f257458e56144e38216d0bf7
Dani <dkotai66@gmail.com>
2022-11-10 14:45:09 +0100

    regex szobor nevre vonatkozoan kesz, form kesz

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let artwork = [];
-let title = document.getElementById('title');
-let year = document.getElementById('year');
-let price = document.getElementById('price');
-let height = document.getElementById('height');
+document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    let title = document.getElementById('title');
+    let year = document.getElementById('year');
+    let price = document.getElementById('price');
+    let height = document.getElementById('height');
+    let errormessage = document.getElementById('errorMessage');
+    let megfeleloE = true;
+    let Regszobornev = /[a-zA-Z]{1,}/;
+    (_a = document.getElementById('send')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        if (!Regszobornev.test(title.value)) {
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor nevében!";
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,21 @@ import { Statute } from "./Statute";
 
 let artwork: Statute[] = [];
 
-let title = document.getElementById('title') as HTMLInputElement;
-let year = document.getElementById('year') as HTMLInputElement;
-let price = document.getElementById('price') as HTMLInputElement;
-let height = document.getElementById('height') as HTMLInputElement;
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    let title = document.getElementById('title') as HTMLInputElement;
+    let year = document.getElementById('year') as HTMLInputElement;
+    let price = document.getElementById('price') as HTMLInputElement;
+    let height = document.getElementById('height') as HTMLInputElement;
+    let errormessage = document.getElementById('errorMessage')as HTMLElement;
+    let megfeleloE = true;
+
+let Regszobornev = /[a-zA-Z]{1,}/;
+    document.getElementById('send')?.addEventListener('click', () => {
+        if(!Regszobornev.test(title.value)){
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor nevében!"
+        }
+    })
+});
