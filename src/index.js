2f0b8b2be7111606c237dca9944f7dc9ad8814b3
Dani <dkotai66@gmail.com>
2022-11-10 15:16:30 +0100

    input ellenorzesek kesz

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+//import { Artwork } from "./artwork";
+//import { Statute } from "./Statute";
 let artwork = [];
+class Statute {
+    constructor(title, year, price, height) {
+        this.title = title;
+        this.year = year;
+        this.price = price;
+        this.height = height;
+    }
+}
 document.addEventListener('DOMContentLoaded', () => {
     var _a;
     let title = document.getElementById('title');
@@ -8,12 +17,29 @@ document.addEventListener('DOMContentLoaded', () => {
     let price = document.getElementById('price');
     let height = document.getElementById('height');
     let errormessage = document.getElementById('errorMessage');
+    let kiiratasihely = document.getElementById('container');
     let megfeleloE = true;
     let Regszobornev = /[a-zA-Z]{1,}/;
+    let Regprice = /[1-9]{1,}/;
     (_a = document.getElementById('send')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
         if (!Regszobornev.test(title.value)) {
             megfeleloE = false;
             errormessage.textContent = "Hiba a szobor nevében!";
+        }
+        if (!Regprice.test(price.value)) {
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor árában!";
+        }
+        if (parseInt(year.value) > 2022) {
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor évében!";
+        }
+        if (parseInt(height.value) < 20) {
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor magasságában!";
+        }
+        if (megfeleloE) {
+            kiiratasihely.textContent = "sikeres felvétel!";
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { Artwork } from "./artwork";
-import { Statute } from "./Statute";
+//import { Artwork } from "./artwork";
+//import { Statute } from "./Statute";
+
+interface Artwork{
+    title: String, 
+    year: number,
+    price: number,
+}
 
 let artwork: Statute[] = [];
 
+class Statute implements Artwork{
+    title: String; 
+    year: number;
+    price: number;
+    height: number;
+
+    constructor(title: String, year: number, price: number, height: number) {
+        this.title = title;
+        this.year = year;
+        this.price = price;
+        this.height = height;
+    }
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,13 +30,32 @@ document.addEventListener('DOMContentLoaded', () => {
     let price = document.getElementById('price') as HTMLInputElement;
     let height = document.getElementById('height') as HTMLInputElement;
     let errormessage = document.getElementById('errorMessage')as HTMLElement;
+    let kiiratasihely = document.getElementById('container') as HTMLDivElement;
     let megfeleloE = true;
 
-let Regszobornev = /[a-zA-Z]{1,}/;
+    let Regszobornev = /[a-zA-Z]{1,}/;
+    let Regprice = /[1-9]{1,}/
+
     document.getElementById('send')?.addEventListener('click', () => {
         if(!Regszobornev.test(title.value)){
             megfeleloE = false;
-            errormessage.textContent = "Hiba a szobor nevében!"
+            errormessage.textContent = "Hiba a szobor nevében!";
+        }
+        if(!Regprice.test(price.value)){
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor árában!";
+        }
+        if(parseInt(year.value) > 2022){
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor évében!";
+        }
+        if(parseInt(height.value) < 20){
+            megfeleloE = false;
+            errormessage.textContent = "Hiba a szobor magasságában!";
+        }
+
+        if(megfeleloE){
+            kiiratasihely.textContent="sikeres felvétel!";
         }
     })
 });
